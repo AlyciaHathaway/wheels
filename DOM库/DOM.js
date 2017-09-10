82a925f76a01a902ffdd2c9af260fadd49bcdaaf
AlyciaHathaway <Alyciahathaways@gmail.com>
2017-09-11 01:23:31 +0800

    step2批量添加事件

--- DOM库/DOM.js
+++ DOM库/DOM.js
@@ -1,4 +1,15 @@
 
 window.$ = function(selector) {
-	return document.querySelectorAll(selector)
+	let array = [];
+	// items是对象，不是数组
+	let items = document.querySelectorAll(selector);
+	for(let i=0; i<items.length; i++){
+		array.push(items[i])
+	}
+	array.on = function(eventType, fn) {
+		for(let i=0; i<array.length; i++){
+			array[i].addEventListener(eventType, fn)
+		}
+	};
+	return array;
 };
