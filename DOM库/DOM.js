814dc71208161f7cc845a9c6f389220adb93c614
AlyciaHathaway <Alyciahathaways@gmail.com>
2017-09-11 09:22:52 +0800

    step3添加addClass方法

--- DOM库/DOM.js
+++ DOM库/DOM.js
@@ -3,12 +3,19 @@ window.$ = function(selector) {
 	let array = [];
 	// items是对象，不是数组
 	let items = document.querySelectorAll(selector);
-	for(let i=0; i<items.length; i++){
+	for (let i=0; i<items.length; i++) {
 		array.push(items[i])
 	}
+	// 添加on方法
 	array.on = function(eventType, fn) {
-		for(let i=0; i<array.length; i++){
+		for (let i=0; i<array.length; i++) {
 			array[i].addEventListener(eventType, fn)
+		}
+	};
+	// 添加addClass方法
+	array.addClass = function(className) {
+		for (let i=0; i<array.length; i++) {
+			array[i].classList.add(className)
 		}
 	};
 	return array;
