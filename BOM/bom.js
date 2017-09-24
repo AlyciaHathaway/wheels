c1b368c44f09ed826c0f995e061059496d66fdbd
AlyciaHathaway <Alyciahathaways@gmail.com>
2017-09-24 16:42:41 +0800

    获取参数

--- BOM/bom.js
+++ BOM/bom.js
@@ -13,6 +13,28 @@ $.bom = {
 			width = ${width}px,height = ${height}px,
 			screenX = ${screen.width/2 - width/2}px, screenY = ${screen.height/2 - height/2}px
 		`)
+	},
+
+	search: function(name, value) {
+		if(value === undefined){
+			let result = {};
+			let search = window.location.search;
+			// 去掉问号
+			if (search[0] === '?') {
+				search = search.slice(1)
+			}
+			// 用&分割成数组
+			let searchArray = search.split('&');
+			// 遍历数组
+			for (let i=0; i<searchArray.length; i++) {
+				let parts = searchArray[i].split('=');
+				// result['a'] === '1'
+				result[decodeURIComponent(parts[0])] = decodeURIComponent(parts[1])
+			}
+			return result[name];
+		}else {
+
+		}
 	}
 };
 
