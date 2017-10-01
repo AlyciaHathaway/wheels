4008dad4ad4831d79932930a6e318950d8299fa5
AlyciaHathaway <Alyciahathaways@gmail.com>
2017-10-01 16:53:26 +0800

    基本功能完成

--- 轮播组件/slides.js
+++ 轮播组件/slides.js
@@ -4,18 +4,27 @@
 // };
 
 window.slides = function(element) {
-	var $el = $(element);
-	var width = $el.width();
-	var count = $el.find('.slide').length;
+	let $el = $(element);
+	let $view = $el.children('.view');
+	let width = $el.width();
+	let count = $el.find('.slide').length;
 
 	// 计算轮播圆点的个数，展示在view层上面
-	var $ol = $('<ol class="controls"></ol>');
+	let $ol = $('<ol class="controls"></ol>');
 	for (let i=0; i<count; i++) {
 		$ol.append(`<li>${i+1}</li>`)
 	}
 	$el.append($ol);
 
-
+	// 轮播切换
+	$ol.on('click', 'li', function(e) {
+		let $li = $(e.currentTarget);
+		let index = $li.index();
+		let number = - width * index;
+		$view.css({
+			transform:  `translateX(${number}px)`
+		})
+	})
 };
 
 
