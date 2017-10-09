cfd2e9e5d3867e05ebf9e5ea516ec0aaf909525a
AlyciaHathaway <Alyciahathaways@gmail.com>
2017-10-09 17:38:36 +0800

    样式和JS计算

--- popover组件/popover.js
+++ popover组件/popover.js
@@ -3,25 +3,38 @@ window.popover = function(options) {
 	let {element, content} = options;
 	let $element = $(element);
 	let $popover;
-	// div创建在外面省CPU，占内存
-	// let $popover = $('<div></div>')
+	show();
+
 	$element.on('click', function() {
 		if ($popover) {
-			// remove只是删除文本和节点，但元素本身依然保留
-			$popover.remove();
+			$popover.remove(); // remove只是删除文本和节点，但元素本身依然保留
 			$popover = null // 关键
 		}else {
-			// 省内存，占CPU
-			$popover = $('<div></div>')
-				.text(content)
-				.insertAfter($element)
+			show();
 		}
 	});
+
+	function show() {
+		let width = $element.outerWidth();
+		let height = $element.outerHeight();
+
+		$popover = $('<div class="popover"></div>') // 省内存，占CPU，如果div创建在外面省CPU，占内存
+													// let $popover = $('<div></div>')
+			.html(content)
+			.insertAfter($element);
+
+		let selfWidth = $popover.outerWidth();
+		let x = - (width/2 + selfWidth/2);
+
+		$popover.css({
+			transform: `translate(${x}px, -100%)`
+		})
+	}
 	return undefined
 };
 
 popover({
 	element: '#btn',
-	content: 'hi',
+	content: 'hi hi hi',
 	trigger: 'click' // 或者 mouse
 });
